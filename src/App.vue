3efb277a2770aab826f9b27d249bfdc430bdf380
Fredric Johansson <fredric@fredricj.se>
2025-12-14 14:02:26 +0100

    Add basic input form

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {type Ref, ref} from "vue";
+
+const names: Ref<FileList | undefined | null> = ref();
+function fileselected(event: Event) {
+  names.value = (event.target as HTMLInputElement).files;
+}
+</script>
 
 <template>
   <h1>Generate paylist</h1>
   <p>
     Generate list of amount to pay for competitors
   </p>
+  <form>
+    <input type="file" name="infiles" multiple @change="fileselected" accept=".xml"/>
+    <div v-if="names && names.length > 0">
+      <p>Selected files:</p>
+      <ul>
+      <li v-for="item in names">
+        {{ item.name }}
+      </li>
+      </ul>
+    </div>
+  </form>
 </template>
 
-<style scoped></style>
+<style scoped>
+form {
+  background-color: lightblue;
+}
+</style>
